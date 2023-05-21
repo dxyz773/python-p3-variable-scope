e075c7c43a323d46d1570514d4170006f8689369
demitryedwards <dxyz773@gmail.com>
2023-05-21 09:58:11 -0400

    complete code-along

---
+++ lib/scope.py
@@ -0,0 +1,10 @@
+change_the_world = "change yourself"
+
+
+def change_yourself():
+    global change_the_world
+    change_the_world = "world changed"
+
+
+change_yourself()
+print(change_the_world)
